2e52fd5107616e1cabb503637c96a4828f13a87d
Mike Bedoya <mikebedoya28@gmail.com>
2024-12-16 23:26:55 +0000

    getting new url

--- src/app.js
+++ src/app.js
@@ -5,28 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function NewCart() {
+  let cardNumbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let cardSymbols = ["♦", "♣", "♠", "♥"];
+  let randomNumber =
+    cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+  let randomSymbol =
+    cardSymbols[Math.floor(Math.random() * cardSymbols.length)];
 
-let cardNumbers = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K"
-];
-let cardSymbols = ["Diamonds", "Clubs", "Spades", "Hearts"];
-let randomNumber = Math.floor(Math.random() * cardNumbers.length);
-let randomSymbol = Math.floor(Math.random() * cardSymbols.length);
-let finalSymbol = cardSymbols[randomSymbol];
-console.log(finalSymbol);
-function getCardsymbol(cardSymbols) {}
+  document.getElementById("symbolUp").innerHTML = randomSymbol;
+  document.getElementById("symbolDown").innerHTML = randomSymbol;
+  document.getElementById("number").innerHTML = randomNumber;
+
+  if (randomSymbol === "♦" || randomSymbol === "♥") {
+    document.getElementById("symbolUp").style.color = "red";
+    document.getElementById("symbolDown").style.color = "red";
+  } else {
+    document.getElementById("symbolUp").style.color = "black";
+    document.getElementById("symbolDown").style.color = "black";
+  }
+};
